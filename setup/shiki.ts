fa1f09bdaaf9e47aef88321c05257fa0e7f5917e
Meghan Gutshall <meghan.gutshall@gmail.com>
2025-04-20 20:36:27 -0400

    Add filename to code snippets

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,4 +1,5 @@
 import { defineShikiSetup } from '@slidev/types'
+import { h } from "hastscript";
 
 export default defineShikiSetup(() => {
   return {
@@ -10,6 +11,27 @@ export default defineShikiSetup(() => {
       'console',
       'rb',
       'sh',
+    ],
+    transformers: [
+      {
+        pre(node) {
+          const rawMeta = this.options.meta?.__raw;
+          const filenameMatch = rawMeta?.match(/filename="([^"]+)"/);
+          const filename = filenameMatch && filenameMatch[1];
+
+          if (!filename) return;
+
+          const div = h(
+            "div",
+            {
+              class: "filename",
+            },
+            filename
+          );
+
+          node.children.unshift(div);
+        },
+      }
     ],
   }
 })
